a5a365dcab03b20860a0c0a505ed345c49b5b598
Abhishek Ambastha <aambasth@cisco.com>
2015-10-27 12:24:56 -0700

    Matrix Rotation Optimized

--- Algorithms/matrixRotation.py
+++ Algorithms/matrixRotation.py
@@ -1,36 +1,41 @@
+inp = raw_input()
+M, N, R = map(int,inp.split())
 
-M =4
-N =4
+mat=[]
+for i in range(M):
+    rows = map(int, raw_input().split())
+    mat.append(rows)
 
 layer = min(M,N)/2
-ans =[]
 matrix=[[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
 
-def rotateAntiClock(matrix,layer):
+def rotateAntiClock(matrix,layer, M,N,R):
 
     for L in range(layer):
-        topLeft = matrix[0+L][0+L]
-        topRight = matrix[0+L][N-L-1]
-        bottomLeft = matrix[M-1-L][0+L]
-        bottomRight = matrix[M-1-L][N-1-L]
-        #top
-        for i in range(L,N-L-1):
-            matrix[L][i] = matrix[L][i+1]
+        rot = R % (2*M+2*N-8*L-4)
+        for i in range(rot):
+            topLeft = matrix[0+L][0+L]
+            topRight = matrix[0+L][N-L-1]
+            bottomLeft = matrix[M-1-L][0+L]
+            bottomRight = matrix[M-1-L][N-1-L]
+            #top
+            for i in range(L,N-L-1):
+                matrix[L][i] = matrix[L][i+1]
 
-        #left
-        for i in range(M-L-1,L,-1):
-            matrix[i][L] = matrix[i-1][L]
-        matrix[L+1][L] = topLeft
+            #left
+            for i in range(M-L-1,L,-1):
+                matrix[i][L] = matrix[i-1][L]
+            matrix[L+1][L] = topLeft
 
-        #bottom
-        for i in range(N-L-1,L,-1):
-            matrix[M-L-1][i]= matrix[M-L-1][i-1]
-        matrix[M-L-1][L+1] = bottomLeft
+            #bottom
+            for i in range(N-L-1,L,-1):
+                matrix[M-L-1][i]= matrix[M-L-1][i-1]
+            matrix[M-L-1][L+1] = bottomLeft
 
-        #right
-        for i in range(L,M-L-1):
-            matrix[i][N-L-1] = matrix[i+1][N-L-1]
-        matrix[M-L-2][N-L-1] = bottomRight
+            #right
+            for i in range(L,M-L-1):
+                matrix[i][N-L-1] = matrix[i+1][N-L-1]
+            matrix[M-L-2][N-L-1] = bottomRight
 
 
     return matrix
@@ -44,7 +49,10 @@ def printMatrix(matrix):
 
 
 
-printMatrix(matrix)
-print "\n"
-ans = rotateAntiClock(matrix,layer)
+#printMatrix(matrix)
+#ans = rotateAntiClock(matrix,layer)
+#printMatrix(ans)
+
+ans = rotateAntiClock(mat,layer,M,N,R)
+
 printMatrix(ans)
